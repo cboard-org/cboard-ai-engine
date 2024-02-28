776aabfe9b3485d7b49d1fbf7d37307c00b5990a
Shay Cojocaru <shayc@outlook.com>
2024-02-28 23:12:39 +0000

    chore: add global-symbols interface

---
+++ src/types/global-symbols.d.ts
@@ -0,0 +1,39 @@
+interface LabelSearchResponse {
+  id: number;
+  text: string;
+  text_diacritised: string;
+  description: string;
+  language: string;
+  picto: Pictogram;
+}
+
+interface Pictogram {
+  id: number;
+  symbolset_id: number;
+  part_of_speech: string;
+  image_url: string;
+  native_format: string;
+  adaptable: string;
+  symbolset: SymbolSet;
+}
+
+interface SymbolSet {
+  id: number;
+  slug: string;
+  name: string;
+  publisher: string;
+  publisher_url: string;
+  status: string;
+  licence: Licence;
+  featured_level: number;
+}
+
+interface Licence {
+  name: string;
+  url: string;
+  version: string;
+  properties: string;
+}
+
+// Assuming the response is an array of LabelSearchResponse
+export type LabelsSearchApiResponse = LabelSearchResponse[];
